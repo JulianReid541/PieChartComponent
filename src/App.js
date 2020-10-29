26bac821dda6ab24ce4124b9ecc2fc9015e8f97c
JulianReid541 <reidjulian@outlook.com>
2020-10-28 17:06:47 -0700

    pie chart renders and mouse over event triggers value name and value

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { rgba } from "polished";
+import { PieChart, Pie } from "recharts";
 
-function App() {
+const Card = styled.div`
+  width: 300px;
+  padding: 2rem 0;
+  border-radius: 3rem;
+  background: #191919;
+`;
+
+const ChartWrapper = styled.div`
+position: relative;
+display: flex;
+justify-content: center;
+`;
+
+const ChartLabel = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 0.875rem;
+  color: ${rgba("white", 0.5)};
+  opacity:
+    ${({ isVisible }) => isVisible ? 1 : 0};
+  text-align: center;
+  transition: opacity 0.25s;
+`;
+
+const ChartLabelValue = styled.div`
+  font-size: 1.75rem;
+  color: ${rgba("white", 0.85)};
+`;
+
+const data = [
+  { 
+    name: "Lost", value: 672, fill: "#292929"
+  },
+  {
+    name: "Strong", value: 462, fill: "#b239dc"
+  },
+  {
+    name: "Moderate", value: 127, fill: "#028ffb"
+  },
+  {
+    name: "Low", value: 41, fill: "#68d4a2"
+  }
+];
+
+const DonutChartCard = ({}) => {
+  const [active, setActive] = useState("");
+  const [mouseOver, setMouseOver] = useState(false);
+  const activeItem = data.find((obj) => obj.name === active);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card>
+      <ChartWrapper>
+        <ChartLabel isVisible={mouseOver}>
+          {activeItem && activeItem.name}
+          <ChartLabelValue>
+            {active !== "" && activeItem.value}
+          </ChartLabelValue>
+        </ChartLabel>
+        <PieChart width={210} height={210}>
+          <Pie
+            isAnimationActive={false}
+            startAngle={90}
+            endAngle={450}
+            strokeWidth={0}
+            data={data}
+            innerRadius={60}
+            outerRadius={80}
+            onMouseEnter={(e) => {
+              setActive(e.name);
+              setMouseOver(true);
+            }}
+            onMouseLeave={(e) => {
+              setMouseOver(false);
+            }}
+          />
+        </PieChart>
+      </ChartWrapper>
+    </Card>
   );
-}
+};
 
-export default App;
+export default DonutChartCard;
